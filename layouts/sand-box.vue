921aca33ec27f1de4216c1c02e783f0463e49ceb
Rodrigo Rufino Elias <rodrigo.rufino.elias@gmail.com>
2021-11-06 16:57:20 -0300

    Inclusão do Vuetify e implementação de menu

--- layouts/sand-box.vue
+++ layouts/sand-box.vue
@@ -1,7 +1,71 @@
 <template>
-  <div>
-    <div>Sand Box - Nuxt Test</div>
-    <Nuxt />
-    <div>Sand Box Footer</div>
-  </div>
+  <v-app>
+    <v-toolbar
+      color="grey lighten-4"
+      dense
+      style="max-height: 48px;"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My files</v-toolbar-title>
+    </v-toolbar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
